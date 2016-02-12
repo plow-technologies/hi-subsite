d4d34729547cb83267d8aaf5fd38a375ba98dc4c
Scott Murphy <scottmurphy09@gmail.com>
2016-02-12 12:06:16 -0600

    updated Build.hs  to build bower repo

--- package-name/Build.hs
+++ package-name/Build.hs
@@ -1,14 +1,22 @@
 #!/usr/bin/env runhaskell
+
 -- stack --resolver lts-5.0 runghc --package turtle
+import           Data.Monoid
 import           Development.Shake
 import           Development.Shake.Command
 import           Development.Shake.FilePath
 import           Development.Shake.Util
-
 main :: IO ()
 main = shakeArgs shakeOptions{shakeFiles="_build"} $ do
            wants
-           "static" %> \_ -> do
-             cmd "git submodule add --name static https://github.com/plow-technologies/onping-static.git ./static"
+           rules
+
   where
-   wants = want ["static","dist"]
+   wants = want ["static","static/js"]
+   rules = staticRule >> jsBuildRule
+   staticRule = "static" %> \_ -> do
+                  cmd "git submodule add --name static https://github.com/plow-technologies/onping-static.git ./static"
+   jsBuildRule = "static/js" %> \out -> do
+                  let bowerjson = takeDirectory1 out </>"bower" <.> "json"
+                  need [bowerjson]
+                  cmd Shell (Cwd (takeDirectory1 out)) "bower install"
